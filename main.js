3a41fe6313e46201ac855c66db09a9d418ce98ee
jorgecasar <jorge.casar@gmail.com>
2011-07-30 21:33:02 +0200

    Reemplazo de más textos

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-function melotiro(){
-	$('.default_message').html("¡Me lo tiro!");
-	$('INPUT[value="Me gusta"]').val("¡Me lo tiro!");
-}
-
-setInterval(function(){
-	melotiro();
-},6000);
-
-melotiro();
+(function melotiro(){
+	$('.like_link, .cmnt_like_link').parent().html(
+		function(index, oldhtml)
+		{
+			return oldhtml.
+				replace('me gusta', 'me lo tiro').
+				replace('Me gusta', 'Me lo tiro').
+				replace('te gusta', 'te lo tiras').
+				replace('os gusta', 'os lo tiráis').
+				replace('le gusta', 'se tira').
+				replace('les gusta', 'se tiran');
+		}
+	);
+	$('.uiUfiLike div div').parent().html(
+		function(index, oldhtml)
+		{
+			return oldhtml.
+				replace('A ', '').
+				replace('le gusta', 'se tira').
+				replace('les gusta', 'se tiran').
+				replace('le gusta', 'se tira').
+				replace('os gusta', 'os tiráis');
+		}
+	);
+	setTimeout(
+		function()
+		{
+			melotiro();
+		},
+		6000
+	);
+})();
